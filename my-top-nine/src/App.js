c1d6cd59d151418c06ecb97ec499cb895067b519
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-03-11 12:55:36 -0600

    added components we will need

--- my-top-nine/src/App.js
+++ my-top-nine/src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Route, NavLink } from 'react-router-dom';
+
+import './css/index.css';
+import ItemBrowse from './components/ItemBrowse.js';
 
 class App extends Component {
+
+  
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          
+          <nav>
+
+          </nav>
         </header>
+        <section className="User">
+
+        </section>
+        <section className="Item-browse">
+
+        </section>
+
+        <Route path="/" />
       </div>
     );
   }
