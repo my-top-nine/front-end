0c497f01ec31a406df83c2fc6bdfb11bf5c52c14
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-03-11 18:15:14 -0600

    added some reactstrap components and styling.

--- my-top-nine/src/components/ItemBrowse.js
+++ my-top-nine/src/components/ItemBrowse.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import User from './User.js';
 
 class ItemBrowse extends Component {
   render() {
     return(
       <div>
-
+        <User />
       </div>
     )
   }
